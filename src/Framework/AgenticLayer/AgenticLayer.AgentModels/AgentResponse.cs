d1d621489b1e14d1dec607d1f32c771b50f0afe4
Pravin Chandankhede <pravinchandankhede@hotmail.com>
2025-07-16 20:12:20 +0530

    added model layer

--- src/Framework/AgenticLayer/AgenticLayer.AgentModels/AgentResponse.cs
+++ src/Framework/AgenticLayer/AgenticLayer.AgentModels/AgentResponse.cs
@@ -4,4 +4,9 @@ public class AgentResponse
 {
     public String Response { get; set; }
     public Boolean IsComplete { get; set; }
+
+    override public String ToString()
+    {
+        return $"Response: {Response}, IsComplete: {IsComplete}";
+    }
 }
